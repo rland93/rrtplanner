68eecc51e138d7a8eb9a8f9d31f8423b946b2dce
Mike Sutherland <msutherl@uci.edu>
2021-12-30 12:52:47 -0800

    vectorized some funcs and made consistent OO interface

--- rrtpp/rrt.py
+++ rrtpp/rrt.py
@@ -1,97 +1,8 @@
 import numpy as np
 import numba as nb
-from matplotlib.collections import LineCollection
 from math import sqrt
 from tqdm import tqdm
 import networkx as nx
-
-
-def get_rrt_LC(T, c="grey"):
-    lines = []
-    for e1, e2 in T.edges():
-        pt1, pt2 = T.nodes[e1]["pos"], T.nodes[e2]["pos"]
-        lines.append((pt1, pt2))
-    return LineCollection(lines, color=c)
-
-
-def get_rand_start_end(world, bias=True):
-    """get random free start, end in the world"""
-    free = np.argwhere(world == 0)
-    """if bias, prefer points far away from one another"""
-    if bias == True:
-        start_i = int(np.random.beta(a=0.5, b=5) * free.shape[0])
-        end_i = int(np.random.beta(a=5, b=0.5) * free.shape[0])
-    else:
-        start_i = np.random.choice(free.shape[0])
-        end_i = np.random.choice(free.shape[0])
-    start = free[start_i, :]
-    end = free[end_i, :]
-    return start, end
-
-
-def sample_all_free(free):
-    """sample uniformly from free space in the world."""
-    return free[np.random.choice(free.shape[0])]
-
-
-def norm(u, v):
-    d = u - v
-    return sqrt(d[0] * d[0] + d[1] * d[1])
-
-
-def collisionfree(world, a, b) -> bool:
-    """calculate linear collision on world between points a, b"""
-    x0 = a[0]
-    y0 = a[1]
-    x1 = b[0]
-    y1 = b[1]
-    dx = abs(x1 - x0)
-    if x0 < x1:
-        sx = 1
-    else:
-        sx = -1
-    dy = -abs(y1 - y0)
-    if y0 < y1:
-        sy = 1
-    else:
-        sy = -1
-    err = dx + dy
-    x0_old, y0_old = x0, y0
-    while True:
-        if world[x0, y0] == 1:
-            return False
-        elif x0 == x1 and y0 == y1:
-            return True
-        else:
-            x0_old, y0_old = x0, y0
-            e2 = 2 * err
-            if e2 >= dy:
-                err += dy
-                x0 += sx
-            if e2 <= dx:
-                err += dx
-                y0 += sy
-
-
-@nb.njit(fastmath=True)
-def r2norm(x):
-    return sqrt(x[0] * x[0] + x[1] * x[1])
-
-
-def nearest(points, x):
-    near = []
-    for p in points:
-        near.append(r2norm(p - x))
-    return np.argmin(near)
-
-
-def near(points, x, r):
-    """find idx of points within r of x"""
-    near = []
-    for i, p in enumerate(points):
-        if r2norm(p - x) < r:
-            near.append(i)
-    return near
 
 
 class RRT(object):
@@ -110,16 +21,93 @@ class RRT(object):
         # set of sampled points
         self.sampled = set()
 
+    @staticmethod
+    def near(points: np.ndarray, x: np.ndarray) -> np.ndarray:
+        # vector from x to all points
+        p2x = points - x
+        # norm of that vector
+        dist = np.linalg.norm(p2x, axis=1)
+        # sorted norms
+        sorted = np.argsort(dist)
+        return sorted
+
+    @staticmethod
+    def within(points: np.ndarray, x: np.ndarray, r: float) -> np.ndarray:
+        # vector from x to all points
+        p2x = points - x
+        # dot dist with self to get r2
+        d2 = p2x[:, 0] * p2x[:, 0] + p2x[:, 1] * p2x[:, 1]
+        # get indices of points within r2
+        near_idx = np.argwhere(d2 < r * r)
+        return np.atleast_1d(np.squeeze(near_idx))
+
+    @staticmethod
+    @nb.njit(fastmath=True)
+    def r2norm(x):
+        return sqrt(x[0] * x[0] + x[1] * x[1])
+
+    @staticmethod
+    @nb.njit()
+    def collisionfree(world, a, b) -> bool:
+        """calculate linear collision on world between points a, b"""
+        x0 = a[0]
+        y0 = a[1]
+        x1 = b[0]
+        y1 = b[1]
+        dx = abs(x1 - x0)
+        if x0 < x1:
+            sx = 1
+        else:
+            sx = -1
+        dy = -abs(y1 - y0)
+        if y0 < y1:
+            sy = 1
+        else:
+            sy = -1
+        err = dx + dy
+        while True:
+            if world[x0, y0] == 1:
+                return False
+            elif x0 == x1 and y0 == y1:
+                return True
+            else:
+                e2 = 2 * err
+                if e2 >= dy:
+                    err += dy
+                    x0 += sx
+                if e2 <= dx:
+                    err += dx
+                    y0 += sy
+
+    def sample_all_free(self):
+        """sample uniformly from free space in the world."""
+        return self.free[np.random.choice(self.free.shape[0])]
+
+    def make(self, xstart: np.ndarray, xgoal: np.ndarray):
+        raise NotImplementedError
+
+    def set_world(self, world: np.ndarray):
+        """update world with new world"""
+        self.world = world
+        self.free = np.argwhere(self.world == 0)
+
+    def set_n(self, n: int):
+        """update n"""
+        self.n = n
+
+    def set_every(self, every: int):
+        """update every"""
+        self.every = every
+
 
 class RRTstar(RRT):
     def __init__(self, world, n, every=100, pbar=True):
         super().__init__(world, n, every=every, pbar=pbar)
 
-    @staticmethod
-    def cost(vcosts, points, v, x):
-        return vcosts[v] + r2norm(points[v] - x)
+    def cost(self, vcosts, points, v, x):
+        return vcosts[v] + self.r2norm(points[v] - x)
 
-    def make(self, xstart, xgoal, r_rewire):
+    def make(self, xstart: np.ndarray, xgoal: np.ndarray, r_rewire):
         points = np.full((self.n, 2), dtype=int, fill_value=1e4)
         vcosts = np.full((self.n,), fill_value=np.inf)
         edges, parents = {}, {}
@@ -135,24 +123,24 @@ class RRTstar(RRT):
             if self.pbar:
                 pbar.update(1)
 
-            xnew = sample_all_free(self.free)
-            vnearest = nearest(points, xnew)
+            xnew = self.sample_all_free()
+            vnearest = self.near(points, xnew)[0]
             xnearest = points[vnearest]
 
-            nocoll = collisionfree(self.world, xnearest, xnew)
+            nocoll = self.collisionfree(self.world, xnearest, xnew)
             if nocoll and tuple(xnew) not in self.sampled:
                 self.sampled.add(tuple(xnew))
 
                 # check least cost path to xnew
                 vbest = vnearest
                 cbest = self.cost(vcosts, points, vbest, xnew)
-                vnear = near(points, xnew, r_rewire)
+                vnear = self.within(points, xnew, r_rewire)
 
                 for vn in vnear:
                     xn = points[vn]
                     cn = self.cost(vcosts, points, vn, xnew)
                     if cn < cbest:
-                        if collisionfree(self.world, xn, xnew):
+                        if self.collisionfree(self.world, xn, xnew):
                             vbest = vn
                             cbest = cn
 
@@ -170,7 +158,7 @@ class RRTstar(RRT):
                     cn = vcosts[vn]
                     cmaybe = self.cost(vcosts, points, vn, xnew)
                     if cmaybe < cn:
-                        if collisionfree(self.world, xn, xnew):
+                        if self.collisionfree(self.world, xn, xnew):
                             parent = parents[vn]
                             if parent is not None:
                                 edges[parent] = None
@@ -187,7 +175,7 @@ class RRTstar(RRT):
         dists = np.linalg.norm(points - xgoal, axis=1)
         # add dist to each point's cost
         for i in np.argsort(vcosts + dists):
-            if collisionfree(self.world, points[i], xgoal):
+            if self.collisionfree(self.world, points[i], xgoal):
                 vgoal = points.shape[0]
                 reached_goal = True
                 edges[vgoal] = i
@@ -196,14 +184,14 @@ class RRTstar(RRT):
 
         # get nearest if not at goal
         if not reached_goal:
-            vgoal = nearest(points, xgoal)
+            vgoal = self.near(points, xgoal)[0]
 
         # build graph
         T = nx.DiGraph()
-        T.add_node(vgoal, pos=xgoal)
+        T.add_node(vgoal, pt=xgoal)
         for i, p in enumerate(points):
-            T.add_node(i, pos=p)
-            T.nodes[i]["pos"]
+            T.add_node(i, pt=p)
+            T.nodes[i]["pt"]
 
         for e2, e1 in edges.items():
             if e1 is not None:
@@ -211,13 +199,14 @@ class RRTstar(RRT):
                     p1, p2 = points[e1], xgoal
                 else:
                     p1, p2 = points[e1], points[e2]
-                dist = r2norm(p2 - p1)
+                dist = self.r2norm(p2 - p1)
                 T.add_edge(e1, e2, dist=dist)
         return T, vgoal
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    from matplotlib.collections import LineCollection
     from world_gen import make_world, get_rand_start_end
 
     w, h = 512, 512
@@ -226,3 +215,13 @@ if __name__ == "__main__":
 
     rrts = RRTstar(world, 1000, every=25, pbar=True)
     T, gv = rrts.make(xstart, xgoal, r_rewire=64)
+
+    lc = []
+    for (e1, e2) in T.edges:
+        p1, p2 = T.nodes[e1]["pt"], T.nodes[e2]["pt"]
+        lc.append([p1, p2])
+
+    fig, ax = plt.subplots()
+    ax.imshow(~world.T, origin="lower", cmap="gray")
+    ax.add_collection(LineCollection(lc, colors="b"))
+    plt.show()
